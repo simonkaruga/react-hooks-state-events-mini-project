d047ff8e3e99762969bfd83c0a178c87c166511b
simonkaruga <simonkaruganjoroge@gmail.com>
2025-10-21 22:31:09 +0300

    Add to origin

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import TaskList from "./TaskList";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+
+  const visibleTasks =
+    selectedCategory === "All"
+      ? tasks
+      : tasks.filter((task) => task.category === selectedCategory);
+
+  
+  function handleDeleteTask(text) {
+    const updatedTasks = tasks.filter((task) => task.text !== text);
+    setTasks(updatedTasks);
+  }
+
+ 
+  function handleAddTask(newTask) {
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <h1>Task List</h1>
+
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onSelectCategory={setSelectedCategory}
+      />
+
+      <NewTaskForm
+        categories={CATEGORIES}
+        onTaskFormSubmit={handleAddTask}
+      />
+
+      <TaskList tasks={visibleTasks} onDeleteTask={handleDeleteTask} />
     </div>
   );
 }
